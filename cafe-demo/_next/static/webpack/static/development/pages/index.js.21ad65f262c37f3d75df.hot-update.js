47d06665195e5baef606a633bc48c251865cd4ce
Ruie Dela Pena <seocored@gmail.com>
2019-07-15 19:38:14 +1000

    Added folder for cafe-demo

---
+++ cafe-demo/_next/static/webpack/static/development/pages/index.js.21ad65f262c37f3d75df.hot-update.js
@@ -0,0 +1,531 @@
+webpackHotUpdate("static\\development\\pages\\index.js",{
+
+/***/ "./components/Menu.js":
+false,
+
+/***/ "./node_modules/@babel/runtime-corejs2/core-js/get-iterator.js":
+false,
+
+/***/ "./node_modules/@babel/runtime-corejs2/core-js/map.js":
+false,
+
+/***/ "./node_modules/@babel/runtime-corejs2/core-js/object/assign.js":
+false,
+
+/***/ "./node_modules/@babel/runtime-corejs2/core-js/object/get-own-property-descriptor.js":
+false,
+
+/***/ "./node_modules/@babel/runtime-corejs2/core-js/object/keys.js":
+false,
+
+/***/ "./node_modules/@babel/runtime-corejs2/core-js/promise.js":
+false,
+
+/***/ "./node_modules/@babel/runtime-corejs2/core-js/reflect/construct.js":
+false,
+
+/***/ "./node_modules/@babel/runtime-corejs2/helpers/esm/arrayWithHoles.js":
+false,
+
+/***/ "./node_modules/@babel/runtime-corejs2/helpers/esm/asyncToGenerator.js":
+false,
+
+/***/ "./node_modules/@babel/runtime-corejs2/helpers/esm/construct.js":
+false,
+
+/***/ "./node_modules/@babel/runtime-corejs2/helpers/esm/iterableToArrayLimit.js":
+false,
+
+/***/ "./node_modules/@babel/runtime-corejs2/helpers/esm/nonIterableRest.js":
+false,
+
+/***/ "./node_modules/@babel/runtime-corejs2/helpers/esm/slicedToArray.js":
+false,
+
+/***/ "./node_modules/@babel/runtime-corejs2/helpers/extends.js":
+false,
+
+/***/ "./node_modules/@babel/runtime-corejs2/helpers/interopRequireWildcard.js":
+false,
+
+/***/ "./node_modules/@babel/runtime-corejs2/regenerator/index.js":
+false,
+
+/***/ "./node_modules/core-js/library/fn/get-iterator.js":
+false,
+
+/***/ "./node_modules/core-js/library/fn/map.js":
+false,
+
+/***/ "./node_modules/core-js/library/fn/object/assign.js":
+false,
+
+/***/ "./node_modules/core-js/library/fn/object/get-own-property-descriptor.js":
+false,
+
+/***/ "./node_modules/core-js/library/fn/object/keys.js":
+false,
+
+/***/ "./node_modules/core-js/library/fn/promise.js":
+false,
+
+/***/ "./node_modules/core-js/library/fn/reflect/construct.js":
+false,
+
+/***/ "./node_modules/core-js/library/modules/_bind.js":
+false,
+
+/***/ "./node_modules/core-js/library/modules/_invoke.js":
+false,
+
+/***/ "./node_modules/core-js/library/modules/_microtask.js":
+false,
+
+/***/ "./node_modules/core-js/library/modules/_new-promise-capability.js":
+false,
+
+/***/ "./node_modules/core-js/library/modules/_object-assign.js":
+false,
+
+/***/ "./node_modules/core-js/library/modules/_perform.js":
+false,
+
+/***/ "./node_modules/core-js/library/modules/_promise-resolve.js":
+false,
+
+/***/ "./node_modules/core-js/library/modules/_species-constructor.js":
+false,
+
+/***/ "./node_modules/core-js/library/modules/_task.js":
+false,
+
+/***/ "./node_modules/core-js/library/modules/_user-agent.js":
+false,
+
+/***/ "./node_modules/core-js/library/modules/core.get-iterator.js":
+false,
+
+/***/ "./node_modules/core-js/library/modules/es6.map.js":
+false,
+
+/***/ "./node_modules/core-js/library/modules/es6.object.assign.js":
+false,
+
+/***/ "./node_modules/core-js/library/modules/es6.object.get-own-property-descriptor.js":
+false,
+
+/***/ "./node_modules/core-js/library/modules/es6.object.keys.js":
+false,
+
+/***/ "./node_modules/core-js/library/modules/es6.promise.js":
+false,
+
+/***/ "./node_modules/core-js/library/modules/es6.reflect.construct.js":
+false,
+
+/***/ "./node_modules/core-js/library/modules/es7.map.from.js":
+false,
+
+/***/ "./node_modules/core-js/library/modules/es7.map.of.js":
+false,
+
+/***/ "./node_modules/core-js/library/modules/es7.map.to-json.js":
+false,
+
+/***/ "./node_modules/core-js/library/modules/es7.promise.finally.js":
+false,
+
+/***/ "./node_modules/core-js/library/modules/es7.promise.try.js":
+false,
+
+/***/ "./node_modules/define-properties/index.js":
+false,
+
+/***/ "./node_modules/function-bind/implementation.js":
+false,
+
+/***/ "./node_modules/function-bind/index.js":
+false,
+
+/***/ "./node_modules/has-symbols/shams.js":
+false,
+
+/***/ "./node_modules/has/src/index.js":
+false,
+
+/***/ "./node_modules/next-server/dist/lib/mitt.js":
+false,
+
+/***/ "./node_modules/next-server/dist/lib/request-context.js":
+false,
+
+/***/ "./node_modules/next-server/dist/lib/router-context.js":
+false,
+
+/***/ "./node_modules/next-server/dist/lib/router/rewrite-url-for-export.js":
+false,
+
+/***/ "./node_modules/next-server/dist/lib/router/router.js":
+false,
+
+/***/ "./node_modules/next-server/dist/lib/router/utils/is-dynamic.js":
+false,
+
+/***/ "./node_modules/next-server/dist/lib/router/utils/route-matcher.js":
+false,
+
+/***/ "./node_modules/next-server/dist/lib/router/utils/route-regex.js":
+false,
+
+/***/ "./node_modules/next-server/dist/lib/utils.js":
+false,
+
+/***/ "./node_modules/next/dist/client/link.js":
+false,
+
+/***/ "./node_modules/next/dist/client/router.js":
+false,
+
+/***/ "./node_modules/next/dist/client/with-router.js":
+false,
+
+/***/ "./node_modules/next/link.js":
+false,
+
+/***/ "./node_modules/object-assign/index.js":
+false,
+
+/***/ "./node_modules/object-keys/implementation.js":
+false,
+
+/***/ "./node_modules/object-keys/index.js":
+false,
+
+/***/ "./node_modules/object-keys/isArguments.js":
+false,
+
+/***/ "./node_modules/object.assign/implementation.js":
+false,
+
+/***/ "./node_modules/object.assign/index.js":
+false,
+
+/***/ "./node_modules/object.assign/polyfill.js":
+false,
+
+/***/ "./node_modules/object.assign/shim.js":
+false,
+
+/***/ "./node_modules/prop-types-exact/build/helpers/isPlainObject.js":
+false,
+
+/***/ "./node_modules/prop-types-exact/build/index.js":
+false,
+
+/***/ "./node_modules/prop-types/checkPropTypes.js":
+false,
+
+/***/ "./node_modules/prop-types/factoryWithTypeCheckers.js":
+false,
+
+/***/ "./node_modules/prop-types/index.js":
+false,
+
+/***/ "./node_modules/prop-types/lib/ReactPropTypesSecret.js":
+false,
+
+/***/ "./node_modules/punycode/punycode.js":
+false,
+
+/***/ "./node_modules/querystring-es3/decode.js":
+false,
+
+/***/ "./node_modules/querystring-es3/encode.js":
+false,
+
+/***/ "./node_modules/querystring-es3/index.js":
+false,
+
+/***/ "./node_modules/react-is/cjs/react-is.development.js":
+false,
+
+/***/ "./node_modules/react-is/index.js":
+false,
+
+/***/ "./node_modules/regenerator-runtime/runtime.js":
+false,
+
+/***/ "./node_modules/url/url.js":
+false,
+
+/***/ "./node_modules/url/util.js":
+false,
+
+/***/ "./node_modules/webpack/buildin/global.js":
+false,
+
+/***/ "./node_modules/webpack/buildin/module.js":
+false,
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var next_head__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! next/head */ "./node_modules/next-server/dist/lib/head.js");
+/* harmony import */ var next_head__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(next_head__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _components_Header__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../components/Header */ "./components/Header.js");
+/* harmony import */ var _components_Footer__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../components/Footer */ "./components/Footer.js");
+/* harmony import */ var _styles_styles_scss__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ../styles/styles.scss */ "./styles/styles.scss");
+/* harmony import */ var _styles_styles_scss__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(_styles_styles_scss__WEBPACK_IMPORTED_MODULE_4__);
+var _jsxFileName = "C:\\Users\\iuiui\\Projects\\demo1\\pages\\index.js";
+
+
+
+
+
+
+var IndexPage = function IndexPage() {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react__WEBPACK_IMPORTED_MODULE_0___default.a.Fragment, null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(next_head__WEBPACK_IMPORTED_MODULE_1___default.a, {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 10
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("title", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 11
+    },
+    __self: this
+  }, "Cafe Mediterranean Redesign"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("meta", {
+    name: "description",
+    content: "The new design of Cafe Mediterranean",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 12
+    },
+    __self: this
+  })), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_Header__WEBPACK_IMPORTED_MODULE_2__["default"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 14
+    },
+    __self: this
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("section", {
+    className: "hero",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 15
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
+    className: "hero-logo",
+    src: "https://thecafemediterranean.com/wp-content/uploads/2018/08/cafe-med-logo-alt.png",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 16
+    },
+    __self: this
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    className: "hero-content",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 20
+    },
+    __self: this
+  }, "The Mediterranean lifestyle is all about enjoying life to the fullest \u2013 through fresh, healthy meals with family and friends, finding a balance between work, relaxation, and experiencing the pleasures of an active life."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "hero-action",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 26
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
+    src: "https://thedarling.com/wp-content/themes/the-darling/img/shell.png",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 27
+    },
+    __self: this
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h3", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 28
+    },
+    __self: this
+  }, "Reservations"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 29
+    },
+    __self: this
+  }, "CALL (02) 720-8757 OR ", react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
+    href: "#",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 30
+    },
+    __self: this
+  }, "BOOK ONLINE")))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("section", {
+    className: "about",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 34
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "about-container",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 35
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "about-circle",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 36
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h3", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 37
+    },
+    __self: this
+  }, "THE RESTAURANT"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 38
+    },
+    __self: this
+  }, "In 1994, two siblings, Gonzalo and Giovanna Mabanta, along with Gonzalo\u2019s then sister-in-law Marla Moran, put their heart, their soul, and their passion for good food and cooking into a restaurant idea that, in those heady days of the 1990s, was still unknown in Manila: a kitchen dedicated to the distinctive cuisines of the sunny Mediterranean."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
+    className: "round-btn",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 46
+    },
+    __self: this
+  }, "Learn More")))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("section", {
+    className: "more",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 50
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "two-columns",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 51
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "left",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 52
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h3", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 53
+    },
+    __self: this
+  }, "TESTIMONIALS"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 54
+    },
+    __self: this
+  }, "My husband loves it so much, he couldn\u2019t share his opinion, he was savoring every bite"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    className: "tagline",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 55
+    },
+    __self: this
+  }, "- Carlomar Frilles")), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "right",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 57
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
+    src: "https://images.pexels.com/photos/5876/food-salad-healthy-vegetables.jpg?auto=compress&cs=tinysrgb&dpr=2&w=2400",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 58
+    },
+    __self: this
+  }))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "two-columns",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 61
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "left",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 62
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h3", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 63
+    },
+    __self: this
+  }, "TESTIMONIALS"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 64
+    },
+    __self: this
+  }, "Service crew were courteous and eating in your place makes me feel I\u2019m in a country in Mediterranean."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    className: "tagline",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 68
+    },
+    __self: this
+  }, "- Camille")), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "right",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 70
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
+    src: "https://images.pexels.com/photos/2122280/pexels-photo-2122280.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=1500",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 71
+    },
+    __self: this
+  })))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_Footer__WEBPACK_IMPORTED_MODULE_3__["default"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 75
+    },
+    __self: this
+  }));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (IndexPage);
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.21ad65f262c37f3d75df.hot-update.js.map
---
+++ cafe-demo/_next/static/webpack/static/development/pages/index.js.b607b0704e5db655ea37.hot-update.js
@@ -0,0 +1,60 @@
+webpackHotUpdate("static\\development\\pages\\index.js",{
+
+/***/ "./components/Header.js":
+/*!******************************!*\
+  !*** ./components/Header.js ***!
+  \******************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "C:\\Users\\iuiui\\Projects\\demo1\\components\\Header.js";
+
+
+var Header = function Header() {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("header", {
+    "class": "header",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 4
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    "class": "header-container",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    "class": "address",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 6
+    },
+    __self: this
+  }, "Greenbelt 1, Ayala Center, Makati City, Metro Manila"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(Menu, {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 9
+    },
+    __self: this
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    "class": "reservation",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 10
+    },
+    __self: this
+  }, "BOOK RESERVATIONS")));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Header);
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.b607b0704e5db655ea37.hot-update.js.map
